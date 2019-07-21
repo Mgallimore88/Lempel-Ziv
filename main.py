c72ccb9e010c22a94f982a2f7cf3c6c48787b3b8
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-07-21 21:21:02 +0100

    Adds template code

---
+++ main.py
@@ -0,0 +1,9 @@
+from encode import encode
+from decode import decode
+
+
+message = "abaabbababaaababa"
+encoded_message = encode(message)
+print(encoded_message)
+decoded_message = decode(encoded_message)
+print(decoded_message)
