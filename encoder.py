33f679c233fa8a7e0fa70a3d004010baa5763396
mgallimore88 <mgallimore88@gmail.com>
2019-07-27 23:10:48 +0100

    continuing large function

--- encoder.py
+++ encoder.py
@@ -10,7 +10,6 @@ class Encoder:
         self.dict_number = 0
 
 
-
         #  turn message string into an iterable list
         for letter in message:
             self.iterable_message.append[letter]
@@ -35,56 +34,9 @@ class Encoder:
                     self.dict_number += 1
 
                     
-                    
-                    
-            
-            
-
-                
-            self.output_message.append(current_word)
-            self.iterable_message.pop(0)
-            self.index = 0
-        while : 
-            self.current_word = self.iterable_message[index]
-            while self.finished is False:
-                self.current_word.append(self.iterable_message[index])
-                #  loop through this whilst rest of message is being sent
-                K = self.iterable_message[index + 1]
-
-                if self.current_word + K in dictionary:
-                    self.current_word.append(K)
-                    self.index += 1
-
-                    
 
 
 
-
-        else:
-            #  iterate to end of message
-            current_wor
-            if str(current_word + k)
-
-
-    
-
-
-
-
-    encode_this_word()
 
     return encoded_message
-
-
-
-def encode_this_word():
-        for i in message:
-
-    if current_word + K is not in dictionary:
-        current_word.append(K)
-
-
-def iterate():
-    i = i + 1
-
 
