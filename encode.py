4f15f4df202d829e0f520c1b8fc0837e9cd0c5a3
mgallimore88 <mgallimore88@gmail.com>
2019-07-27 17:19:16 +0100

    Adding incomplete encoder

--- encode.py
+++ encode.py
@@ -1,2 +1,48 @@
-def encode(message):
+class encoder:
+    def __init__(self, input_message):
+        self.input message = input_message
+        self.rest_of_string = input_message
+        self.iterable_message = []
+
+        for letter in message:
+            iterable_message.append[letter]
+        
+    def encode(message):
+        #  check for start of message: initialise lists
+        if len(self.input_message) == len(self.rest_of_string):
+            # do on receipt of message
+            self.index = 0
+            K = message[self.index]
+            dictionary = []
+            dictionary.append(K)
+            self.output_message.append(current_word)
+
+
+            
+        else:
+            #  iterate to end of message
+            if str(current_word + k)
+
+
+    
+
+
+
+
+    encode_this_word()
+
     return message
+
+
+
+def encode_this_word():
+        for i in message:
+
+    if current_word + K is not in dictionary:
+        current_word.append(K)
+
+
+def iterate():
+    i = i + 1
+
+
