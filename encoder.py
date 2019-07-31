7f1bccd5c8f54c69a64085202fe34b24f2d61d40
mgallimore88 <mgallimore88@gmail.com>
2019-07-31 16:25:57 +0100

    updated encode. unfixed bug following indexes and searching for end of file

--- encoder.py
+++ encoder.py
@@ -1,5 +1,5 @@
 class Encoder:
-    def __init__(self, input_message):
+    def __init__(self, input_message, EOF_symbol='!'):
         self.input_message = input_message
         self.rest_of_string = input_message
         self.iterable_input = []
@@ -15,42 +15,58 @@ class Encoder:
     def encode(self):
         #  do on receipt of message
         self.running = True
-        self.current_word.append(self.iterable_input[self.index])
+        self.current_word = self.iterable_input[self.index]
         #  Loop through this until end of message
         while self.running:
-            print(self.current_word)
+            print("***")
+            print(f"index = {self.index}")
+            print(f"encoded message = {self.encoded_message_}")
+            print(f"current dictionary = {self.dictionary}")
+            print(f"current word = {self.current_word}")
+
             #  K = next letter
+            self.check_for_end()
+            if self.running is False:
+                return self.encoded_message_
             K = self.iterable_input[self.index + 1]
-            if K == "!":
-                self.running = False
-            if (self.current_word[0]) not in self.dictionary:
+
+            if str(self.current_word) not in self.dictionary:
                 #  case for single letter not in dict:
                 self.encoded_message_.append(self.current_word)
                 print(f"{self.current_word} added to output string")
-                self.dictionary[self.current_word[0]] = self.dict_number
+                self.dictionary[self.current_word] = self.dict_number
+                print(f"{self.current_word[0]} added to dictionary at {self.dict_number}")
                 self.dict_number += 1
                 self.index += 1
                 self.current_word = self.iterable_input[self.index]
-            else:  #  self.current_word[0] in self.dictionary:
+
+            else:  # single letter is in the dictionary:
                 #  check whether next letter makes a new word
                 if (self.current_word[0] + K) in self.dictionary:
                     print("adding next letter to current word")
                     #  add next letter to current word and start again
-                    self.current_word[0].append(K)
+                    self.current_word += K
                     print(f"old word in dict. new word = {self.current_word}")
                     self.index += 1
-                elif (self.current_word[0] + K) not in self.dictionary:
-                    print("found a new word")
+                elif (self.current_word + K) not in self.dictionary:
+                    print("adding next letter makes a new word")
                     #  case for new word encountered
-                    #  Add to dict, output corresponding dict CODE + K, reset curent word
-                    dict_KEY = self.dictionary.get((self.current_word[0]))
+                    #  Add to dict, output corresponding dict CODE + K, reset current word
+                    dict_KEY = self.dictionary.get(self.current_word)
                     self.encoded_message_.append(str(dict_KEY) + str(K))
                     print(f"{str(dict_KEY) + str(K)} added to output string")
                     self.dictionary[self.current_word[0] + K] = self.dict_number
                     self.dict_number += 1
-                    self.index += 1
+                    #  increase index by 2 since new word is current + 1
+                    #  so next unparsed letter is current + 2
+                    self.check_for_end()
+                    self.index += 2
                     self.current_word = self.iterable_input[self.index]
-                    print(f"new dictionary = {self.dictionary}")
-        print(f"FINAL dictionary {self.dictionary}")
-        return self.encoded_message_
-        
+
+    def check_for_end(self):
+        if (self.iterable_input[self.index] == "!"
+            or self.iterable_input[self.index + 1] == "!"):
+            self.running = False
+            print(f"Final dictionary {self.dictionary}")
+            return self.encoded_message_
+
