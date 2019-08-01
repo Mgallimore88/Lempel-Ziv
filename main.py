e0ef85e81ac022edbcafd84b745bfe93c08c2ddb
mgallimore88 <mgallimore88@gmail.com>
2019-08-01 16:16:36 +0100

    Swedish message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from encode import encode
 from decoding import decode
 
 
-message = "aaa!"
+message = "abbaABBAabbaABBA!"
 encoded_message = encode(message)
 print(f"Encoded message:{encoded_message}")
 decoded_message = decode(encoded_message)
