ad61081c5fbb0e0be3e73af3889c3a1719e9b80d
mgallimore88 <mgallimore88@gmail.com>
2019-08-09 23:38:30 +0100

    Organising code in encoder.py after discussing code style with Alex

--- encoder.py
+++ encoder.py
@@ -5,73 +5,71 @@ class Encoder:
         self.dictionary = {}
         self.index = 0
         self.dict_number = 0
-        self.debug = True
-
-    def encode(self):
-        #  do on receipt of message
-        self.running = True
+        self.debug = False
+        self.is_finished = False
         self.current_word = self.input_message[self.index]
 
-        while self.running:  # Loop until end of message
+    def encode(self):
+
+        while True:  # Loop until end of message
             self.next_letter = self.input_message[self.index + 1]
-            self.print_status()
             self.check_for_end()
-            if self.running is False:
+            if self.is_finished:
                 self.encoded_message.append(self.current_word)
                 break
 
             #  current word not encountered before:
             if str(self.current_word) not in self.dictionary:
                 self.encoded_message.append(self.current_word)
-                print(f"{self.current_word} added to output string")
+                # print(f"{self.current_word} added to output string")
                 self.dictionary[self.current_word] = self.dict_number
-                print(f"{self.current_word} added to dictionary at {self.dict_number}")
+                # print(f"{self.current_word} added to dictionary at {self.dict_number}")
                 self.dict_number += 1
                 self.check_for_end()
-                if self.running is False:
+                if self.is_finished:
                     break
                 self.index += 1
                 self.current_word = self.input_message[self.index]
 
-            else:  # current word encountered before:
+            # Current word encountered before:
+            elif (self.current_word + self.next_letter) in self.dictionary:
                 #  check whether next letter makes a new word
-                if (self.current_word + self.next_letter) in self.dictionary:
                     #  add next letter to current word and start again
-                    print("Adding next letter to current word")
+                    # print("Adding next letter to current word")
                     self.current_word += self.next_letter
-                    print(f"Old word in dict. New word = {self.current_word}")
+                    # print(f"Old word in dict. New word = {self.current_word}")
                     self.index += 1
                     self.check_for_end()
-                    if self.running is False:
+                    if self.is_finished:
                         self.encoded_message.append(self.current_word)
                         break
 
-                elif (self.current_word + self.next_letter) not in self.dictionary:
-                    # Next letter makes a new word.
-                    # Write current word's dict KEY + next letter to output.
-                    # Add current word + next letter to dictionary.
-                    # Reset current word.
-                    print("adding next letter makes a new word")
-                    dict_KEY = self.dictionary.get(self.current_word)
-                    self.encoded_message.append(str(dict_KEY) + self.next_letter)
-                    print(f"{str(dict_KEY) + self.next_letter} added to output string")
-                    self.dictionary[
-                        self.current_word + self.next_letter
-                    ] = self.dict_number
-                    self.dict_number += 1
-                    self.check_for_end(2)  # next unparsed letter is n + 2
-                    if self.running is False:
-                        break
-                    self.index += 2
-                    self.current_word = self.input_message[self.index]
+            else:
+                # Adding next letter to current word makes a new word.
+                dict_KEY = self.dictionary.get(self.current_word)
+                # Write current word's dict KEY + next letter to output.
+                self.encoded_message.append(str(dict_KEY) + self.next_letter)
+                # print(f"{str(dict_KEY) + self.next_letter} added to output string")
+                # Add current word + next letter to dictionary.
+                self.dictionary[
+                    self.current_word + self.next_letter
+                ] = self.dict_number
+                self.dict_number += 1
+                self.check_for_end(2)  # next unparsed letter is n + 2
+                if self.is_finished:
+                    break
+                self.index += 2
+                # Reset current word.
+                self.current_word = self.input_message[self.index]
 
         print(f"Final dictionary {self.dictionary}")
         return ''.join(self.encoded_message)
 
     def check_for_end(self, index_increment=1):
         if self.input_message[self.index + index_increment] == "!":
-            self.running = False
+            self.is_finished = True
 
+        self.print_status()
     def print_status(self):
         if self.debug is True:
             print("***")
