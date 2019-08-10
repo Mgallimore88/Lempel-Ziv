c0d110d8de92acabeae5891542eec3a448221cc5
mgallimore88 <mgallimore88@gmail.com>
2019-08-10 19:49:27 +0100

    adding pipes functionality to mainsys

--- mainsys.py
+++ mainsys.py
@@ -1,11 +1,13 @@
+#! /usr/bin/python3
+
 from encode import encode
 from decode import decode
 from sys import stdin as input, stdout as output
 
-message = "ahhhhhhhhhhhhh!!"
+message = "Hello from inside mainsys"
 
 output.write(encode(input.read()))
-
-print(f"Encoded message:{encoded_message}")
+encoded_message = encode(message)
+print(f"Encoded message: {encoded_message}")
 decoded_message = decode(encoded_message)
-print(decoded_message)
+print(f"Decoded message:{decoded_message}")
