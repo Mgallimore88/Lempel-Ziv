f2c32fd3b925835b3fa982bcb58421a9798f0824
mgallimore88 <mgallimore88@gmail.com>
2019-08-11 14:50:05 +0100

    changing test message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from encode import encode
 from decoding import decode
 
 
-message = "Compresses text with spaces!"
+message = "test"
 encoded_message = encode(message)
 print(f"Encoded: {encoded_message}")
 decoded_message = decode(encoded_message)
